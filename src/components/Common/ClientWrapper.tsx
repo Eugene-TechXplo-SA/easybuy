e9058435f254fd8d306d6cf886bf9d89a6bacf05
Eugene-TechXplo-SA <eugene@techxplo.co.za>
2026-04-09 16:56:37 +0200

    Updated package.json

---
+++ src/components/Common/ClientWrapper.tsx
@@ -0,0 +1,47 @@
+"use client";
+import { useState, useEffect } from "react";
+import { ModalProvider } from "@/app/context/QuickViewModalContext";
+import { CartModalProvider } from "@/app/context/CartSidebarModalContext";
+import { ReduxProvider } from "@/redux/provider";
+import QuickViewModal from "@/components/Common/QuickViewModal";
+import CartSidebarModal from "@/components/Common/CartSidebarModal";
+import { PreviewSliderProvider } from "@/app/context/PreviewSliderContext";
+import PreviewSliderModal from "@/components/Common/PreviewSlider";
+import ScrollToTop from "@/components/Common/ScrollToTop";
+import PreLoader from "@/components/Common/PreLoader";
+import Header from "@/components/Header";
+import Footer from "@/components/Footer";
+
+export default function ClientWrapper({ children }: { children: React.ReactNode }) {
+  const [loading, setLoading] = useState<boolean>(true);
+
+  useEffect(() => {
+    setTimeout(() => setLoading(false), 1000);
+  }, []);
+
+  return (
+    <>
+      {loading ? (
+        <PreLoader />
+      ) : (
+        <>
+          <ReduxProvider>
+            <CartModalProvider>
+              <ModalProvider>
+                <PreviewSliderProvider>
+                  <Header />
+                  {children}
+                  <QuickViewModal />
+                  <CartSidebarModal />
+                  <PreviewSliderModal />
+                </PreviewSliderProvider>
+              </ModalProvider>
+            </CartModalProvider>
+          </ReduxProvider>
+          <ScrollToTop />
+          <Footer />
+        </>
+      )}
+    </>
+  );
+}
